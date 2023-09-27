f2c029abac3905064a2bc1498101cbb5fc3a99ba
Joe Futrelle <jfutrelle@whoi.edu>
2023-09-27 14:33:51 -0400

    rename col: station -> nearest_station

--- web/api/workflows.py
+++ web/api/workflows.py
@@ -16,7 +16,7 @@ def add_nearest_station(df):
         # get the nearest station
         nearest_location = Station.nearest_location(row['latitude'], row['longitude'], row['timestamp'])
         # add the station's id to the row
-        df.at[index, 'station'] = nearest_location.get_station().name
+        df.at[index, 'nearest_station'] = nearest_location.get_station().name
         df.at[index, 'distance_km'] = nearest_location.distance.m / 1000 # convert to km
 
     return df
