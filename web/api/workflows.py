b1e83cd2d79322ed3a306674a9ddd3f2f9e3b57a
Joe Futrelle <jfutrelle@whoi.edu>
2023-10-02 11:01:42 -0400

    add error handling

--- web/api/workflows.py
+++ web/api/workflows.py
@@ -6,12 +6,16 @@ from api.models import Station
 
 def add_nearest_station(input_df, timestamp_column=None, latitude_column=None, longitude_column=None):
 
+    # look for standard column names if none are provided
     df = regularize_column_names(input_df, {
         'latitude': ['lat', 'lt'] if latitude_column is None else [latitude_column],
         'longitude': ['lon', 'long', 'lng', 'lg'] if longitude_column is None else [longitude_column],
         'timestamp': ['time', 'datetime', 'date'] if timestamp_column is None else [timestamp_column]
     })
 
+    if not {'latitude', 'longitude', 'timestamp'}.issubset(df.columns):
+        raise ValueError('Missing one or more required columns: latitude, longitude, timestamp')
+    
     for index, row in df.iterrows():
         # get the nearest station
         nearest_location = Station.nearest_location(row['latitude'], row['longitude'], row['timestamp'])
@@ -19,6 +23,7 @@ def add_nearest_station(input_df, timestamp_column=None, latitude_column=None, l
         df.at[index, 'nearest_station'] = nearest_location.get_station().name
         df.at[index, 'distance_km'] = nearest_location.distance.m / 1000 # convert to km
 
+    # use original column names for existing columns
     df.columns = list(input_df.columns) + ['nearest_station', 'distance_km']
 
     return df
