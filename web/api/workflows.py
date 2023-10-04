2e8b42e936133d527e244a1b390e1cbb63f85d85
Joe Futrelle <jfutrelle@whoi.edu>
2023-10-04 12:07:03 -0400

    adding lat/lon range check

--- web/api/workflows.py
+++ web/api/workflows.py
@@ -21,6 +21,11 @@ def add_nearest_station(input_df, timestamp_column=None, latitude_column=None, l
         raise ValueError('Cannot overwrite existing columns: nearest_station, distance_km')
 
     for index, row in df.iterrows():
+        # check lat/lon for out of range values
+        if row['latitude'] < -90 or row['latitude'] > 90:
+            raise ValueError(f'Invalid latitude value: {row["latitude"]}')
+        if row['longitude'] < -180 or row['longitude'] > 180:
+            raise ValueError(f'Invalid longitude value: {row["longitude"]}')
         # get the nearest station
         nearest_location = Station.nearest_location(row['latitude'], row['longitude'], row['timestamp'])
         # add the station's id to the row
